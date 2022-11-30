dc2f3ad495973130d8384ee3874156110cb2304e
ADMIN <thebao123417@gmail.com>
2022-11-30 10:40:58 +0700

    commit deploy heroku

--- src/components/Panel/GroupOwned.js
+++ src/components/Panel/GroupOwned.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import PanelHero from "../General/PanelHero";
 import GroupDetail from "../Modals/GroupDetail";
 import groupApi from "../../api/GroupApi";
-import jwt from "jwt-decode";
+import jwt from 'jwt-decode'
 
 function GroupOwned({token}) {
     const [groupId, setGroupId] = useState();
